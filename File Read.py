0fc68b73b58dfd3ef457e9afdab127a5fd556f4e
mahyargigi <mahyarghoraian@gmail.com>
2019-07-18 15:05:56 +0430

    first commit

---
+++ File Read.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+import xlrd,os
+all_datas = pd.Series()
+for i in os.listdir('./excels'):
+    xls = xlrd.open_workbook("./excels/"+str(i) , on_demand=True)
+    for j in xls.sheet_names():
+        file = pd.read_excel("./excels/"+str(i) , j)
+        file = pd.DataFrame(data= file).T
+        file = file.iloc[1:,]
+        file.columns = ['date' , 'market cap' , 'return']
+        file['name'] = j
+        for t in range(1,13):
+            file[t] = np.NaN
+            for u in range(t,len(file)):
+               file[t].iloc[u] = file["return"].iloc[u-t]
+        for t in range(24,37,12):
+            file[t] = np.NaN
+            for u in range(t,len(file)):
+               file[t].iloc[u] = file["return"].iloc[u-t]
+
+        all_datas = pd.concat([all_datas , file] , sort=False)
+print(all_datas)
